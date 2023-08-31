48cb56ad199062fe7322a40a763032eb7417ed8b
fazleyelahi <fazleyelehi@gmail.com>
2023-08-31 19:28:32 +0600

    .

--- TechTopia/TechTopia.ino
+++ TechTopia/TechTopia.ino
@@ -5,7 +5,7 @@ void setup() {
   display.begin(SSD1306_EXTERNALVCC, 0x3C);
   display.clearDisplay();
   display.setTextSize(2);
-  display.setTextColor(WHITE);
+  display.setTextColor(SSD1306_WHITE);
   TechTopia();
 }
 
