30e3f4b7eb3e063ae3fcde4e7b62d984be41503c
fazleyelahi <fazleyelehi@gmail.com>
2023-10-28 20:35:22 +0600

    Update TechTopia.ino

--- TechTopia/TechTopia.ino
+++ TechTopia/TechTopia.ino
@@ -1,18 +1,14 @@
 #include <Adafruit_SSD1306.h>
 #include <Wire.h>
-Adafruit_SSD1306 display(128, 32, &Wire, -1);
+Adafruit_SSD1306 display(128, 64, &Wire, -1);
 
 void setup() {
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
   display.clearDisplay();
   display.setTextSize(2);
   display.setTextColor(SSD1306_WHITE);
-  // display.setRotation(1);
-  // TechTopia();
-  display.setCursor(0, 0);
-  display.print("HELLO");
+  TechTopia();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 }
